7fa4423ca2d2bec7a8f2ece47c33e04be4f5b79e
vijay8077 <vijays.aussizz@gmail.com>
2020-03-07 09:35:28 +0530

    test

--- test-api-app/src/App.js
+++ test-api-app/src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ListData from './component/listdata';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React! Direct change in master branch Vijay</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ListData></ListData>
   );
 }
 
---
+++ test-api-app/src/component/listdata.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ListData extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                Wel Come
+            </div>
+          );
+    }
+}
+ 
+export default ListData;
